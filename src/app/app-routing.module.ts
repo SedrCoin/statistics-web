7bedcab4598134331161652b1e61fc66f7ec854f
artem <aas0804@mail.ru>
2024-06-21 18:27:24 +0300

    fixed path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { MainComponent } from './views/main/main.component';
 import { AuthGuard } from './services/auth.guard';
 
 export const routes: Routes = [
-	{ path: '', redirectTo: '/main', pathMatch: 'full' },
+	{ path: '',  component: MainComponent, pathMatch: 'full' },
 	{ path: 'main', component: BaseViewComponent, pathMatch: 'full' },
 	// { path: 'main', component: BaseViewComponent, pathMatch: 'full', canActivate: [AuthGuard] },
 	{ path: 'welcome', component: MainComponent, pathMatch: 'full' },
